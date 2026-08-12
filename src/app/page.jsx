7f1dce146995b0905885e8044f2a3676d28b28d4
Filips-source <filipedupin13@gmail.com>
2026-08-12 10:40:16 -0300

    First commit

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from 'next/image';
+
+
+export default function Home() {
+  return (
+    <main className={styles.content}>
+      <Image src />
+        <h1>oi</h1>
+    </main>
+  );
+}
---
+++ src/app/sobre/page.jsx
@@ -0,0 +1,9 @@
+import "./sobre.css";
+
+export default function sobre(){
+    return(
+        <>
+            <h1>Pagina sobre funciona!</h1>
+        </>
+    )
+};
