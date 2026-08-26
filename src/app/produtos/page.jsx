ead5826c626065470c2784a581181b08e18e6ea7
Filips-source <filipedupin13@gmail.com>
2026-08-26 09:16:29 -0300

    Ajusta pagina de produtos e imagens

---
+++ src/app/produtos/page.jsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import CardProduto from "@/components/cardProduto";
+import "./produtos.css"
+
+export default function ProdutosPage() {
+    const listaProdutos = [
+        {
+            id: 1,
+            titulo: "Notebook ideapad Gaming",
+            descricao: "Um notebook gamer para seus melhores jogos",
+            imagem: "/imagens/ideapad.png",
+            preco: 3999.0,
+        },
+        {
+            id: 2,
+            titulo: "Monitor",
+            descricao: "180hz HDR e IPS perfeito para jogar",
+            imagem: "/imagens/monitor.png",
+            preco: 987.2,
+        },
+        {
+            id: 3,
+            titulo: "Mouse MX Logitech",
+            descricao: "Tecnologia de ponta para sua experiencia",
+            imagem: "/imagens/mouse.png",
+            preco: 687.0,
+        },
+        {
+            id: 4,
+            titulo: "Teclado Wouting",
+            descricao: "Teclado 0 delay, sem input lag para trabalhar e jogar",
+            imagem: "/imagens/teclado.png",
+            preco: 1700.0,
+        },
+    ];
+
+    return (
+        <>
+            <Header />
+            <h1>Listagem de produtos</h1>
+            <div className="card-container">
+
+            {listaProdutos.map((produto) => (
+                <CardProduto
+                key={produto.id}
+                titulo={produto.titulo}
+                descricao={produto.descricao}
+                imagem={produto.imagem}
+                preco={produto.preco}
+                />
+            ))}
+            </div>
+            <Footer />
+        </>
+    );
+}
+
